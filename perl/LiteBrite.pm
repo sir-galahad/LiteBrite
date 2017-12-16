923e5a4f68f12224c7e4426315a0dd3d81a5d18f
Aaron Davis <aaron.davis@plixer.com>
2017-12-16 13:23:44 -0500

    added a perl function to get a colors hash and the perl module now exports all functions

--- perl/LiteBrite.pm
+++ perl/LiteBrite.pm
@@ -1,5 +1,42 @@
-package LiteBrite;
 require XSLoader;
+require Exporter;
 
+package LiteBrite;
 XSLoader::load('LiteBrite');
+
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(
+    liteb_init
+    liteb_clear
+    liteb_update
+    liteb_setblock
+    liteb_get_maxx
+    liteb_get_maxy
+    liteb_getkey
+    liteb_exit
+    liteb_getcolors
+);
+
+sub liteb_getcolors{
+    my %colors = (
+	black => 0,
+        bblack => 1,
+	red => 2,
+        bred => 3,
+	green => 4,
+        bgreen => 5,
+	yellow => 6,
+        byellow => 7,
+	blue => 8,
+        bblue => 9,
+	purple => 10,
+        bpurple => 11,
+	cyan => 12,
+        bcyan => 13,
+	white => 14,
+        bwhite => 15,
+    );
+    return %colors;
+}
+
 1;
--- perl/test.pl
+++ perl/test.pl
@@ -1,11 +1,23 @@
 #!/usr/bin/perl
 
 use strict;
-use LiteBrite;
+use LiteBrite qw(
+    liteb_init
+    liteb_clear
+    liteb_setblock
+    liteb_update
+    liteb_exit
+    liteb_getkey
+    liteb_get_maxx
+    liteb_get_maxy
+    liteb_getcolors
+);
+    
 
-&LiteBrite::liteb_init();
-&LiteBrite::liteb_clear(0);
-&LiteBrite::liteb_setblock(10,10,5);
+liteb_init();
+my %colors=liteb_getcolors();
+liteb_clear($colors{blue});
+liteb_setblock(10,10,$colors{green});
 &LiteBrite::liteb_update();
 
 while(1){
